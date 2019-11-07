775e906ef930c481645cf4982f273d4d96fd92d0
op-labs <56323800+op-labs@users.noreply.github.com>
2019-11-07 14:35:47 -0700

    Update kickoff.py

--- kickoff.py
+++ kickoff.py
@@ -10,13 +10,21 @@ def httpReq(method,url,authHeader,showStatus=False):
 			print('\tItem Running')
 		elif res.status_code == 202:
 			print('\tItem Queued to Run')
+		elif res.status_code == 403:
+			print('\tRequest Returned Status 403')
+		elif res.status_code == 404:
+			print('\tRequest Returned Status 404')
 	return json.loads(res.content)
 
-apiKey =  sys.argv[1]
-folderId = sys.argv[2]
+try:
+	apiKey =  str(sys.argv[1])
+	folderId = int(sys.argv[2])
+except:
+	print('Invalid Inputs')
+	sys.exit(1)
+
 baseURL = 'https://api.observepoint.com/v2'
 authHeader = {'Authorization': 'api_key ' + apiKey}
-
 domainIds = []
 
 # Makes a list of all domains that are in the desired folder
@@ -30,17 +38,19 @@ for dom in domainIds:
 	auditData = httpReq('get',baseURL + '/domains/' + str(dom) + '/web-audits', authHeader)
 	for audit in auditData:
 		# Runs the audit if it isn't currently running
-		print('Audit ' + str(audit['id']) + ':')
+		print('Audit ' + str(audit['id']) + ' (' + audit['name'] + '):')
 		if (audit['webAuditRunning'] == False):
 			auditRunData = httpReq('post',baseURL + '/web-audits/' + str(audit['id']) + '/runs', authHeader,True)
 		else:
-			print('Item Already Running')
+			print('\tItem Already Running')
 
 # Gets a list of all the journeys from the above folder/domains
 for dom in domainIds:
 	journeyData = httpReq('get',baseURL + '/domains/' + str(dom) + '/web-journeys', authHeader)
 	for journey in journeyData:
 		# Runs the journey if it isn't currently running
+		print('Journey ' + str(journey['id']) + ' (' + journey['name'] + '):')
 		if (journey['webJourneyRunning'] == False):
-			print('Running Journey ' + str(journey['id']) + ':')
 			journeyRunData = httpReq('post',baseURL + '/web-journeys/' + str(journey['id']) + '/runs', authHeader,True)
+		else:
+			print('\tItem Already Running')
